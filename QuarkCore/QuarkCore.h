e14ca143dec9a018a799438a78717c11f8aa7c47
Nathan Flurry <bitbucket@nathanflurry.com>
2016-10-12 16:08:16 -0700

    Initial Commit

---
+++ QuarkCore/QuarkCore.h
@@ -0,0 +1,19 @@
+//
+//  QuarkCore.h
+//  QuarkCore
+//
+//  Created by Nathan Flurry on 10/12/16.
+//  Copyright © 2016 Vesto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QuarkCore.
+FOUNDATION_EXPORT double QuarkCoreVersionNumber;
+
+//! Project version string for QuarkCore.
+FOUNDATION_EXPORT const unsigned char QuarkCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuarkCore/PublicHeader.h>
+
+
