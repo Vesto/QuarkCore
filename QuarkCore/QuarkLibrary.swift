6cc787ba1086bdf3871e974d44f2e7c371d346bf
Nathan Flurry <bitbucket@nathanflurry.com>
2016-11-17 19:14:38 -0700

    Now `QuarkLibrary` imports from a single built file instead of enumerating all the JavaScript files

--- QuarkCore/QuarkLibrary.swift
+++ QuarkCore/QuarkLibrary.swift
@@ -10,40 +10,25 @@ import AppKit
 
 public enum QKLibraryError: Error {
     case failedToFindLibrary
-    case failedToEnumerateLibrary
 }
 
 public class QuarkLibrary {
-    /// Returns the paths for each JavaScript resource.
-    public static func getLibraryFiles() throws -> [URL] {
+    /// Returns the URL to the built library file.
+    public static func getLibrary() throws -> URL {
         // Get the proper bundle
         // Use this instead of `Bundle(identifier: "com.Vest.QuarkCore")`
         // it doesn't force a specific bundle ID
         let bundle = Bundle(for: QuarkLibrary.self)
         
         // Get the URL of the library folder
-        guard let libraryURL = bundle.url(forResource: "Quark Library", withExtension: nil) else {
+        guard let buildUrl = bundle.url(
+            forResource: "build",
+            withExtension: "js",
+            subdirectory: "Quark Library/dist"
+        ) else {
             throw QKLibraryError.failedToFindLibrary
         }
         
-        // Get the enumerator for the file maanger
-        guard
-            let enumerator = FileManager.default.enumerator(
-                at: libraryURL,
-                includingPropertiesForKeys: [],
-                options: .skipsHiddenFiles,
-                errorHandler: { _, _ in true } // Do nothing
-            )
-        else {
-            throw QKLibraryError.failedToEnumerateLibrary
-        }
-        
-        // Filter out the JS files
-        let jsFiles = enumerator
-            .map { $0 as? URL } // Safely map to URL
-            .filter { $0?.pathExtension == "js" } // Filter to JS files and non-nil
-            .map { $0! } // Unwrap all values (safe because of `filter`)
-        
-        return jsFiles
+        return buildUrl
     }
 }
